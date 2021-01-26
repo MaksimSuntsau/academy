9d3cd90e9a4362620f668dd2f6a33c73a0151e0d
MaksimSuntsau <suntsevmaksim@gmail.com>
2021-01-26 14:15:52 +0300

    Homework2

--- academy/src/by/academy/homework/hw2/Task1.java
+++ academy/src/by/academy/homework/hw2/Task1.java
@@ -6,13 +6,13 @@ package by.academy.homework.hw2;
 
 		public static void main(String[] args) {
 
-			System.out.print("Vvedite summu pokupki : ");
+			System.out.print("Enter purchase amount : ");
 
 			Scanner scan = new Scanner(System.in);
 
 			double sum = scan.nextDouble();
 
-			System.out.print("Vvedite vozrast : ");
+			System.out.print("Enter age : ");
 
 			int age = scan.nextInt();
 
@@ -20,36 +20,36 @@ package by.academy.homework.hw2;
 
 			if (sum < 100) {
 
-				System.out.println("Itogo s uchetom skidki 5% : " + (sum - sum * 0.05));
+				System.out.println("Total including discount 5% : " + (sum - sum * 0.05));
 			}
 
 			if (sum >= 100 & sum < 200) {
 
-				System.out.println("Itogo s uchetom skidki 7% : " + (sum - sum * 0.07));
+				System.out.println("Total including discount 7% : " + (sum - sum * 0.07));
 			}
 
 			if (sum >= 200 & sum < 300) {
 
-				System.out.println("Itogo s uchetom skidki 12% : " + (sum - sum * 0.12));
+				System.out.println("Total including discount 12% : " + (sum - sum * 0.12));
 
 				if (age > 18) {
 
-					System.out.println("Bonus za sovershennoletie +4% k skidke : " + (sum - sum * 0.16));
+					System.out.println("Bonus for majority +4% to discount : " + (sum - sum * 0.16));
 
 				} else {
 
-					System.out.println("Antibonus nesovershennoletnim -3% ot skidki: " + (sum - sum * 0.09));
+					System.out.println("Antibonus for minority -3% from discount: " + (sum - sum * 0.09));
 				}
 			}
 
 			if (sum >= 300 & sum < 400) {
 
-				System.out.println("Itogo s uchetom skidki 15% : " + (sum - sum * 0.15));
+				System.out.println("Total including discount 15% : " + (sum - sum * 0.15));
 			}
 
 			if (sum >= 400) {
 
-				System.out.println("Itogo s uchetom skidki 20% : " + (sum - sum * 0.2));
+				System.out.println("Total including discount 20% : " + (sum - sum * 0.2));
 			}
 
 		}
