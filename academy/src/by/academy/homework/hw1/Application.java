7b7f3efed03f0fdc59bfb2639aad834beed5f1d5
MaksimSuntsau <suntsevmaksim@gmail.com>
2021-01-26 12:50:40 +0300

    Homework1

---
+++ academy/src/by/academy/homework/hw1/Application.java
@@ -0,0 +1,30 @@
+package by.academy.homework.hw1;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Cat cat1= new Cat();
+		cat1.nickname = "Мaksik";
+		System.out.println("Имя первого кота:" + cat1.nickname);
+		cat1.eat();
+		cat1.sleep();
+		cat1.walk();
+		
+		Cat cat2 = new Cat();
+		cat2.age=5;
+		for (int i = 0; i < 3; i++)
+		{
+			cat2.grow();
+			System.out.println("Возраст второго кота:" + cat2.getAge());
+		}
+		
+		cat1.setMoney(50);
+		cat2.setMoney(30);
+		System.out.println(cat1.getMoney()+cat2.getMoney());
+		
+		cat1.getInitials();
+		
+		cat1.IsHomeAnimal(cat1.setIsHomeAnimal());
+		}	
+
+	}
---
+++ academy/src/by/academy/homework/hw1/Cat.java
@@ -0,0 +1,75 @@
+package by.academy.homework.hw1;
+
+import java.util.Scanner;
+
+public class Cat {
+	double money;
+	int age;
+	String nickname;
+	char initials;
+	boolean isHomeAnimal;
+
+	public Cat() {
+	}
+
+	public Cat(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public void grow() {
+		age = age + 1;
+	}
+
+	public void sleep() {
+		System.out.println("Кот спит");
+	}
+
+	public void eat() {
+		System.out.println("Кот ест");
+	}
+
+	public void walk() {
+		System.out.println("Кот гуляет");
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setMoney(double money) {
+		this.money = money;
+	}
+
+	public double getMoney() {
+		return money;
+	}
+
+	public char setInitials() {
+
+		this.initials = nickname.charAt(0);
+		return initials;
+	}
+
+	public void getInitials() {
+		System.out.println(setInitials());
+	}
+
+	public boolean setIsHomeAnimal() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Это домашнее животное? 1-да, 0-нет ");
+		int num = scan.nextInt();
+
+		scan.close();
+
+		if (num == 1)
+			isHomeAnimal = true;
+		return isHomeAnimal;
+	}
+
+	public void IsHomeAnimal(boolean IsHomeAnimal) {
+		if (IsHomeAnimal == true)
+			System.out.print("Это домашнее животное!");
+		else if (IsHomeAnimal == false)
+			System.out.print("Это не домашнее животное!");
+	}
+}
